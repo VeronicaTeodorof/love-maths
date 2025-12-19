1cdc35311551b4e1d80eb48b7038a9b9f45555ea
VeronicaTeodorof <vteodorof@rocketmail.com>
2025-12-19 17:22:03 +0000

    Tidy up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,19 @@ document.addEventListener("DOMContentLoaded", function() {
             }
             });
         }
-
         runGame("addition")
     });
+
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     // Create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
